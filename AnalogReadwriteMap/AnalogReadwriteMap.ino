d27f8fa6ce09e2fdeac85540eec616129285f5ad
Nikolay Stankov <nikolay.g.stankov@gmail.com>
2025-05-18 21:28:19 -0400

    Adding files

---
+++ AnalogReadwriteMap/AnalogReadwriteMap.ino
@@ -0,0 +1,19 @@
+int ledPin = 13;
+int analogPin = 1;
+int val = 0;
+void setup() {
+  pinMode (ledPin, OUTPUT);
+}
+  
+ void loop() {
+  // read the input on analog pin 0:
+  val = analogRead(analogPin);
+  val= map(val, 1023, 0 ,0 ,255);
+  
+digitalWrite(ledPin,HIGH);
+  delay(val/4);   
+digitalWrite (ledPin,LOW);
+delay(val/20);
+}
+
+
